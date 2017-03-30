5350aea673078ba9fe83db26068d68e63dd6f4bd
vitormalencar <vitor.maia.alencar@gmail.com>
2017-03-30 02:23:53 -0300

    add get current time using tracked decorator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css'
-
 import {h, Component} from 'preact'
-
+import tracked from 'tracked';
 export default class App extends Component {
-  render() {
-    return <div className="App">
-      <div className="App-heading App-flex">
-        <h2>
-          Welcome to <img alt="Preact" src={require('./preact-name.svg')} style="height: 1.8em; vertical-align: middle;"/>
-        </h2>
-      </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./preact-logo.svg')}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
-    </div>
-  }
+
+	@tracked time = 0;
+
+	componentDidMount() {
+		this.loadTime();
+	}
+
+	async loadTime() {
+		let response = await fetch(`https://script.googleusercontent.com/macros/echo?user_content_key=4eq2-Kc22KTPKScJcIpT7QwWTm0ulvURnhUAvQ5uoj56XCM3m8uRCYl8ckU9BqSoS7-VVhJI-gEBortQlZWku9MsRqJO0Mdam5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJ9GRkcRevgjTvo8Dc32iw_BLJPcPfRdVKhJT5HNzQuXEeN3QFwl2n0M6ZmO-h7C6bwVq0tbM60-_IQDS8gp7-yKHe_C4eJUZEKxBMsj9Z72&lib=MwxUjRcLr2qLlnVOLh12wSNkqcO1Ikdrk`)
+		this.time = await response.json();
+    setTimeout(()=>{this.loadTime()},1000);
+	};
+
+	render() {
+		return (
+			<div class="time">
+				<h2>Current Time</h2>
+				<adress>Fortaleza CE, Brazil</adress>
+				<div>
+          <strong>{this.time.hours} - {this.time.minutes} - {this.time.seconds}</strong>
+				</div>
+			</div>
+		)
+	}
 }
